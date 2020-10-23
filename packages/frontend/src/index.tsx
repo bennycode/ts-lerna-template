063da7793c1b9c75c52bda2936c85c888f830bc3
Benny Neugebauer <bn@bennyn.de>
2020-10-23 16:51:27 +0200

    feat(frontend): Use local TypeScript packages

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {PriceCalculator} from '@tstv/business';
 
-const App = () => <h1>Hello, World!</h1>;
+const DivWrapper = (props: React.HTMLProps<HTMLDivElement>) => <div {...props} />;
+
+const App = () => <>
+  <h1>Hello, {PriceCalculator.getTotal(100)}!</h1>
+  <DivWrapper children={<span>Hello, World!</span>}/>
+</>;
 const rootNode = document.getElementById('root');
 
 ReactDOM.render(
